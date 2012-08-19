b352cb01d31430356d62d153a229709851a63ad4
Tomasz Milczarek <miltomasz@mac>
2012-08-19 15:30:55 +0200

    initial commit

---
+++ mustache_generator.rb
@@ -0,0 +1,9 @@
+class MustacheGenarator
+	MUSTACHES = ['           ##   ', '         #######', 
+								'   #   #########', '   ###########  ', '    ##          ']
+	def generate
+		MUSTACHES.each { |m| puts "#{m} #{m.reverse}" }
+	end
+end
+
+MustacheGenarator.new.generate
